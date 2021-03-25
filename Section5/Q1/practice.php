49ebf978e7133d5ca9210152f1a8e84620f54a35
Koju Sakaguchi <fuetf@icloud.com>
2021-03-25 15:00:01 +0900

    練習問題１

---
+++ Section5/Q1/practice.php
@@ -0,0 +1,228 @@
+<?php
+  // Q1
+  echo 'Q1';
+  echo '<br>';
+
+  $name = 'sakaguchi';
+  echo '私の名前は「' . $name . '」です。';
+  echo '<br>';
+  echo '<br>';
+
+  //Q2
+  echo 'Q2';
+  echo '<br>';
+
+  $num = 5 * 4;
+  $num1 = $num / 2;
+  echo $num;
+  echo '<br>';
+  echo $num1;
+  echo '<br>';
+  echo '<br>';
+
+  // Q3
+  echo 'Q3';
+  echo '<br>';
+
+
+  $currentTime = new DateTime(); // DateTime class
+  $time = $currentTime->format('Y年m月d日 H時i分s秒'); //format method
+  echo '現在時刻は、' . $time . 'です。';
+  echo '<br>';
+  echo '<br>';
+
+  // Q4
+  echo 'Q4';
+  echo '<br>';
+
+  $age = 25;
+  if ($age >= 20) {
+      echo '成人です。';
+      echo '<br>';
+  } else {
+      echo '未成年です。';
+  }
+  echo '<br>';
+
+  // Q5
+  echo 'Q5';
+  echo '<br>';
+
+  $device = 'mac';
+
+  if ($device === 'mac') {
+      echo '使用OSは、macです。';
+  }  else {
+      if ($device === 'windows') {
+          echo '使用OSは、windowsです。';
+      }
+        echo 'どちらでもありません。';
+    }
+
+  echo '<br>';
+  echo '<br>';
+
+  //Q6
+  echo 'Q6';
+  echo '<br>';
+
+  $kantoArea = ['東京', '千葉', '埼玉', '神奈川', '群馬', '栃木', '茨城'];
+  echo "$kantoArea[5]県と$kantoArea[1]県は関東地方の都道府県です。";
+  echo '<br>';
+  echo '<br>';
+
+  //Q7
+  echo 'Q7';
+  echo '<br>';
+
+  $area = ['東京' => '新宿区', '神奈川' => '横浜市', '千葉' => '千葉市', '埼玉' => 'さいたま市', '栃木' => '宇都宮市', '群馬' =>'前橋市', '茨城' => '水戸市'];
+  foreach ($area as $value) {
+      echo $value;
+      echo '<br>';
+  }
+  echo '<br>';
+
+  //Q8
+  echo 'Q8';
+  echo '<br>';
+
+  foreach ($area as $prefecture => $city) {
+    if ($prefecture === '埼玉'){
+        echo $prefecture . '県の県庁所在地は、' . $city. 'です。';
+        echo '<br>';
+    }
+  }
+  echo '<br>';
+
+  //Q9
+  echo 'Q9';
+  echo '<br>';
+
+  $prefecturalCapital = ['東京' => '新宿区', '神奈川' => '横浜市', '千葉' => '千葉市', '埼玉' => 'さいたま市', '栃木' => '宇都宮市', '群馬' =>'前橋市', '茨城' => '水戸市', '愛知' => '愛知市', '大阪' => '大阪市'];
+  foreach ($prefecturalCapital as $prefecture => $capital) {
+      if ($prefecture === '愛知') {
+          echo $prefecture . '県は関東地方ではありません。';
+          echo '<br>';
+      } else {
+          if ($prefecture === '大阪') {
+              echo $prefecture . '府は関東地方ではありません。';
+              return false;
+          }
+            echo $prefecture . '県の県庁所在地は、' . $capital . 'です。';
+            echo '<br>';
+      }
+  }
+  echo '<br>';
+
+  //Q10
+  echo 'Q10';
+  echo '<br>';
+
+  function hello($name) {
+    echo $name. 'さん、こんにちは。';
+  }
+  hello('金谷');
+  echo '<br>';
+
+  hello('安藤');
+  echo '<br>';
+  echo '<br>';
+
+  //Q11
+  echo 'Q11';
+  echo '<br>';
+
+  function calcTaxInPrice($price) {
+    $taxInPrice =  1.1 * $price;
+    echo $price . '円の商品の税込価格は' . $taxInPrice . "円です。";
+    echo '<br>';
+  }
+  calcTaxInPrice(1000);
+  echo '<br>';
+
+  //Q12
+  echo 'Q12';
+  echo '<br>';
+
+  function distinguishNum($num) {
+    if ($num % 2 === 0) {
+        echo $num . 'は偶数です。';
+    } else {
+        echo $num . 'は奇数です。';
+    }
+  }
+
+  distinguishNum(11);
+  echo '<br>';
+  distinguishNum(22);
+  echo '<br>';
+  echo '<br>';
+
+  //Q13
+  echo 'Q13';
+  echo '<br>';
+
+  function evaluateGrade($grade) {
+    switch ($grade) {
+        case 'A':
+        case 'B':
+            echo '合格です。';
+            break;
+
+        case 'C':
+            echo '合格ですが追加課題があります。';
+            break;
+
+        case 'D':
+            echo '不合格です。';
+            break;
+
+        default:
+            echo '判定不明です。講師に問い合わせてください。';
+            break;
+    }
+  }
+  evaluateGrade('A');
+  echo '<br>';
+  evaluateGrade('F');
+  echo '<br>';
+  echo '<br>';
+
+  //Q14
+  echo 'Q14';
+  echo '<br>';
+
+  class Student
+  {
+      public $studentId;
+      public $name;
+
+      public function __construct($studentId, $name)
+      {
+          $this->studentId = $studentId;
+          $this->name = $name;
+      }
+
+      public function studentInfo()
+      {
+          echo '学籍番号' . $this->studentId . '番の生徒は' . $this->name . 'です。';
+      }
+
+      public function attend($language)
+      {
+          echo $this->name . 'は' . $language .'の授業に参加しました。学籍番号:' . "\t" . $this->studentId;
+      }
+  }
+
+  $yamada = new Student(120, '山田');
+  $yamada->studentInfo();
+  echo '<br>';
+  echo '<br>';
+
+  //Q15
+  echo 'Q15';
+  echo '<br>';
+
+  $yamada->attend('PHP');
+
+?>
---
+++ Section5/Q2/practice2.php
@@ -0,0 +1,108 @@
+<?php
+//Q1
+echo 'Q1';
+echo '<br>';
+
+for ($i = 1; $i <= 100; $i++) {
+  if ($i % 3 === 0 && $i % 5 === 0)
+  {
+      echo 'FizzBuzz';
+      echo '<br>';
+  } elseif ($i % 5 === 0)
+  {
+      echo 'Buzz';
+      echo '<br>';
+  } elseif ($i % 3 === 0)
+  {
+      echo 'Fizz';
+      echo '<br>';
+  } else
+  {
+      echo $i;
+      echo '<br>';
+  }
+}
+echo '<br>';
+
+//Q2
+echo 'Q2-1';
+echo '<br>';
+
+$personalInfos = [
+  [
+      'name' => 'Aさん',
+      'mail' => 'aaa@mail.com',
+      'tel'  => '09011112222'
+  ],
+  [
+      'name' => 'Bさん',
+      'mail' => 'bbb@mail.com',
+      'tel'  => '08033334444'
+  ],
+  [
+      'name' => 'Cさん',
+      'mail' => 'ccc@mail.com',
+      'tel'  => '09055556666'
+  ],
+];
+
+echo $personalInfos[1]['name']. 'の電話番号は' . $personalInfos[1]['tel'] . 'です。';
+echo '<br>';
+echo '<br>';
+
+echo 'Q2-2';
+echo '<br>';
+
+$number = 0;
+foreach ($personalInfos as $person ) {
+  $number +=1;
+    echo   $number . '番目の' . $person['name'] . 'のメールアドレスは' . $person['mail'] . 'で、電話番号は' . $person['tel'] . 'です。';
+    echo '<br>';
+}
+echo '<br>';
+
+echo 'Q2-3';
+echo '<br>';
+
+$ageList = [25, 30, 18];
+
+// foreach ($personalInfos as $person ) {
+//     array_push($personalInfos[], $ageList);
+// }
+
+// foreach ($ageList as $age) {
+//   echo $age;
+//   echo '<br>';
+// }
+
+// array_push($personalInfos, $ageList);
+
+// var_dump($personalInfos);
+
+//Q3
+echo 'Q3-1';
+echo '<br>';
+
+$date = new DateTime();  //property
+
+$last_date = new DateTime(date('Y-m-d', strtotime('-1 month', $date->getTimestamp())));
+$last_date = $last_date->format('Y-m-d');
+
+$prev_date = $date->modify('-1 month')->format('Y-m-d');
+
+if ($last_date < $prev_date) {
+    echo $last_date;
+} else {
+    echo $prev_date;
+}
+
+echo 'Q3-2';
+echo '<br>';
+
+$date_time = new DateTime('1992-04-10');
+$now = new DateTime();
+
+$diff = $date_time->diff($now);
+
+echo $diff->format('あの日から%a日経過しました。'); //a??
+//echo $diff->days;
